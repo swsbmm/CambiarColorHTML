7033c84166d48d165416779ab14cb2668fd1d366
swsbmm <swsbmm@gmail.com>
2021-02-09 19:29:37 -0500

    version 0.1

---
+++ logica.js
@@ -0,0 +1,14 @@
+
+function colorRed(){
+    document.body.style.backgroundColor = 'red'
+}
+
+
+function colorBlue(){
+    document.body.style.backgroundColor = 'blue'
+}
+
+
+function colorTomato(){
+    document.body.style.backgroundColor = 'tomato'
+}
